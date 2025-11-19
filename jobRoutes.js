7fc158e8ef071e56dd3772980fa475ae2a86a14a
Shreya-patil12 <Shreyapatil8765@gmail.com>
2025-11-19 19:17:49 +0530

    Update jobRoutes.js

--- jobRoutes.js
+++ jobRoutes.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const routes = express.Router();
-const jobController = require('../controllers/jobController');
-const authMiddleware = require('../middleware/authMiddleware');
-const roleMiddleware = require('../middleware/roleMiddleware');
+const jobController = require('./jobcontroller');
 
-// List all jobs (public)
-routes.get('/', jobController.listJobs);
-
-// Create job (recruiter only)
-routes.post('/', authMiddleware, roleMiddleware('recruiter'), jobController.createJob);
-
-// Update job (recruiter only, their own jobs)
-routes.put('/:id', authMiddleware, roleMiddleware('recruiter'), jobController.updateJob);
-
-// Delete job (recruiter only, their own jobs)
-routes.delete('/:id', authMiddleware, roleMiddleware('recruiter'), jobController.deleteJob);
+routes.post('/', jobController.createJob);
+routes.get('/', jobController.getAllJobs);
+routes.get('/:id', jobController.getJobById);
+routes.put('/:id', jobController.updateJob);
+routes.delete('/:id', jobController.deleteJob);
 
 module.exports = routes;
-
-
