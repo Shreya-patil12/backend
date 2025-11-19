324bb17fbc4861e7914280a8ac2416c71b77e55b
Shreya-patil12 <Shreyapatil8765@gmail.com>
2025-11-19 18:25:49 +0530

    Update applicationroutes.js

--- applicationroutes.js
+++ applicationroutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Routes();
 const applicationController = require('../controllers/applicationController');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
 // Student applies to job
-router.post('/apply', authMiddleware, roleMiddleware('student'), applicationController.applyToJob);
+routes.post('/apply', authMiddleware, roleMiddleware('student'), applicationController.applyToJob);
 
 // Recruiter views applications for a job
-router.get('/job/:jobId', authMiddleware, roleMiddleware('recruiter'), applicationController.getApplicationsForJob);
+routes.get('/job/:jobId', authMiddleware, roleMiddleware('recruiter'), applicationController.getApplicationsForJob);
 
 // Student views applied jobs
-router.get('/my-applications', authMiddleware, roleMiddleware('student'), applicationController.getAppliedJobsForStudent);
+routes.get('/my-applications', authMiddleware, roleMiddleware('student'), applicationController.getAppliedJobsForStudent);
 
-module.exports = router;
+module.exports = routes;
+
