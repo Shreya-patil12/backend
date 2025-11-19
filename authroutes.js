2d3611a37dcbcf5346bf74d7233ece9cb2f3b4dc
Shreya-patil12 <Shreyapatil8765@gmail.com>
2025-11-19 18:19:30 +0530

    Update authroutes.js

--- authroutes.js
+++ authroutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Routes();
 const authController = require('../controllers/authController');
 
 // Register new user
-router.post('/register', authController.register);
+routes.post('/register', authController.register);
 
 // Login user
-router.post('/login', authController.login);
+routes.post('/login', authController.login);
 
-module.exports = router;
+module.exports = routes;
