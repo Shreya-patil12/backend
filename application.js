891cf0074d7a97facde35093cbe76da3ac502147
Shreya-patil12 <Shreyapatil8765@gmail.com>
2025-11-19 19:22:46 +0530

    Update application.js

--- application.js
+++ application.js
@@ -1,27 +1,10 @@
-// models/Application.js
-
 const mongoose = require('mongoose');
 
-// Define the schema for the Application collection
-const ApplicationSchema = new mongoose.Schema({
-  student: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  job: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Job',
-    required: true
-  },
-  resumeUrl: {
-    type: String,
-    required: true
-  },
-  appliedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+const applicationSchema = new mongoose.Schema({
+  jobId: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
+  applicantName: String,
+  applicantEmail: String,
+  resumeUrl: String,
+}, { timestamps: true });
 
-module.exports = mongoose.model('Application', ApplicationSchema);
+module.exports = mongoose.model('Application', applicationSchema);
