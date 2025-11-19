ee498fce20fb4062abfc2436414203b80e7c0cc9
Shreya-patil12 <Shreyapatil8765@gmail.com>
2025-11-19 19:16:31 +0530

    Update authroutes.js

--- authroutes.js
+++ authroutes.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const routes = express.Router();  // Corrected from 'Routes()' to 'Router()'
+const routes = express.Router();
 
 const authController = require('./authcontroller');
 
-
-
-// Register new user
 routes.post('/register', authController.register);
-
-// Login user
 routes.post('/login', authController.login);
 
 module.exports = routes;
-
-
