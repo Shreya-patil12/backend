91f8510c69adb13ac6a21c1e50fecbbaf2d58e0b
Shreya-patil12 <Shreyapatil8765@gmail.com>
2025-11-19 19:20:41 +0530

    Update job.js

--- job.js
+++ job.js
@@ -1,45 +1,11 @@
-// models/Job.js
-
 const mongoose = require('mongoose');
 
-// Define the schema for the Job collection
-const JobSchema = new mongoose.Schema({
-  company: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    required: true
-  },
-  type: {
-    type: String,
-    enum: ['Internship', 'Full-time', 'Part-time'],
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  minCGPA: {
-    type: Number,
-    required: true
-  },
-  requiredSkills: {
-    type: [String],
-    default: []
-  },
-  description: {
-    type: String
-  },
-  postedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  postedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+const jobSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  company: String,
+  location: String,
+  salary: Number,
+}, { timestamps: true });
 
-module.exports = mongoose.model('Job', JobSchema);
+module.exports = mongoose.model('Job', jobSchema);
